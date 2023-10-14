bd3676a2febde37c70603653bb864498f709a3c5
YuvrajChauhan1303 <email.yuvraj.13@gmail.com>
2023-10-14 20:07:38 +0530

    Defined classes for Vertex and Edge

--- Lab Assignment 5/graph-input/graph_input.cpp
+++ Lab Assignment 5/graph-input/graph_input.cpp
@@ -1,1 +1,43 @@
 #include "graph_input.hpp"
+
+class vertex
+{
+    private:
+        char label;
+        int numberOfEdges;
+        edge ** edgesArray;
+    public:
+        vertex(char label);
+        void createEdgesArray(int numberOfEdges);
+};
+
+class edge
+{
+    private:
+        std::string label;
+        vertex * fromVertex;
+        vertex * toVertex;
+    public:
+        edge(std::string label);
+        void createEdge(vertex * from, vertex * to);
+};
+
+//Function Declarations for Vertex Class:
+
+vertex::vertex(char label)
+{
+    this->label = label;
+}
+
+void vertex::createEdgesArray(int numberOfEdges = 0)
+{
+    this->numberOfEdges = numberOfEdges;
+    this->edgesArray = new edge * [numberOfEdges];
+}
+
+//Function Declarations for Edge Class:
+
+edge::edge(std::string label)
+{
+    this->label = label;
+}
--- Lab Assignment 5/graph-input/graph_input.hpp
+++ Lab Assignment 5/graph-input/graph_input.hpp
@@ -1,1 +1,13 @@
 #include <iostream>
+#include <string>
+
+//Class Declarations
+
+class vertex;
+class edge;
+
+/*
+    1. Create an Array of Objects for Vertex.
+    2. for each object in vertex.array[]
+        2.1 ask for number of edges
+*/
